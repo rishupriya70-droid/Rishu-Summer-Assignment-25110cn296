171f5f3b8bad137f3a4c8376c0e16ff95d670f06
Rishu <rishupriya70@gmail.com>
2026-06-01 19:01:55 +0530

    Added Main.java assignment

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a = 1;
+        while (n>a){
+            System.out.println(a);
+            a++;
+        }
+        input.close();
+    }
     
 }
