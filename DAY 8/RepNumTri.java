24adabcc2f14f9958e0930ef144fa73313e226c2
Rishu <rishupriya70@gmail.com>
2026-06-08 21:39:22 +0530

    ORGANISED

---
+++ DAY 8/RepNumTri.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class RepNumTri {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = input.nextInt();
+        System.out.println("The pattern is:");
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(i);
+            }
+        System.out.println();      
+        }
+        input.close();    
+    }
+}
