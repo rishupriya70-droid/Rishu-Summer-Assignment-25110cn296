9c9186b0d6ff406e5128383dc87c9a8ef5916a2d
Rishu <rishupriya70@gmail.com>
2026-06-09 21:14:23 +0530

    ORGANISED

---
+++ DAY 9/RevNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RevNum {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = input.nextInt();
+        System.out.println("The pattern is:");
+        for(int i = n; i >= 1; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+        System.out.println();      
+        }
+        input.close();    
+    }
+    
+}
